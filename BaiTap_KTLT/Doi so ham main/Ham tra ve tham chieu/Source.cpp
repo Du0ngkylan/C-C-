21433775212ae5097d1b01c409bb13dc580c1f2a
Du0ngkylan <duongxuanmai.k55tt@gmail.com>
2020-05-27 16:30:03 +0900

    up

--- BaiTap_KTLT/Doi so ham main/Ham tra ve tham chieu/Source.cpp
+++ BaiTap_KTLT/Doi so ham main/Ham tra ve tham chieu/Source.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
 
-
-
-int& Getx(int& x)
-{
-	return x;
+int& Getx(int& x) {
+  	return x;
 }
 
-int main()
-{
-	int x;
-	Getx(x) = 5; // x = 5
+int main() {
+	int x = 5;
+	auto x1 = Getx(x); // x1= 5
+	Getx(x) = 10; // x = 10 vì sao lại vậy nhể?
 	cout<<Getx(x);
 	system("pause");
 	return 0;
